8d7255db4344f97c95eab7daf918bdbc4b11ddc8
LeonEstrak <aniket2583@gmail.com>
2021-07-21 07:34:49 +0530

    Removed uneccessary comments

--- main.py
+++ main.py
@@ -11,12 +11,6 @@ import re
 import string
 from nltk import stem
 from nltk.corpus import twitter_samples
-##print (twitter_samples.fileids())
-'''
-Output:
- 
-['negative_tweets.json', 'positive_tweets.json', 'tweets.20150430-223406.json']
-'''
 
 pos_tweets = twitter_samples.strings('positive_tweets.json')
 # print (len(pos_tweets)) # Output: 5000
@@ -26,10 +20,6 @@ neg_tweets = twitter_samples.strings('negative_tweets.json')
 
 all_tweets = twitter_samples.strings('tweets.20150430-223406.json')
 # print (len(all_tweets)) # Output: 20000
-
-#---------------------------------------------------------------------#
-# from nltk.tokenize import TweetTokenizer
-# tweet_tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 
 #---------------------------------------------------------------------#
 
@@ -82,8 +72,8 @@ def clean_tweets(tweet) -> list:
     for word in tweet_tokens:
         if (word not in stopwords_english and  # remove stopwords
             word not in emoticons and  # remove emoticons
-            word not in string.punctuation and
-                re.fullmatch(re.compile(r"[A-Za-z]+"), word)):  # remove punctuation
+            word not in string.punctuation and  # remove punctuation
+                re.fullmatch(re.compile(r"[A-Za-z]+"), word)): 
             stem_word = stemmer.stem(word)  # stemming word
             tweets_clean.append(stem_word)
 
@@ -96,20 +86,6 @@ def clean_tweets(tweet) -> list:
 
     return tweets_clean
 
-# #print (pos_tweets[5])
-# '''
-# Output:
-
-# @BhaktisBanter @PallaviRuhail This one is irresistible :)
-# #FlipkartFashionFriday http://t.co/EbZ0L2VENM
-# '''
-
-# #print (clean_tweets(pos_tweets[5]))
-# '''
-# Output:
-
-# ['one', 'irresistible', 'flipkartfashionfriday']
-# '''
 
 #---------------------------------------------------------------------#
 
@@ -123,8 +99,10 @@ def bag_of_words(tweet):
 
 
 custom_tweet = "RT @Twitter Hello There! Have a great day. :) #good #morning"
-#print (bag_of_words(custom_tweet))
+
 '''
+print (bag_of_words(custom_tweet))
+
 Output:
  
 {'great': True, 'good': True, 'morning': True, 'hello': True, 'day': True}
@@ -140,18 +118,11 @@ neg_tweets_set = []
 for tweet in neg_tweets:
     neg_tweets_set.append((bag_of_words(tweet), 'neg'))
 
-# print (len(pos_tweets_set), len(neg_tweets_set)) # Output: (5000, 5000)
-
-#---------------------------------------------------------------------#
-# radomize pos_reviews_set and neg_reviews_set
-# doing so will output different accuracy result everytime we run the program
 shuffle(pos_tweets_set)
 shuffle(neg_tweets_set)
 
 test_set = pos_tweets_set[:1000] + neg_tweets_set[:1000]
 train_set = pos_tweets_set[1000:] + neg_tweets_set[1000:]
-
-# print(len(test_set),  len(train_set)) # Output: (2000, 8000)
 
 #---------------------------------------------------------------------#
 
@@ -170,8 +141,6 @@ MaxEntAccuracy = classify.accuracy(MaxEntClassifier, test_set)
 print("Maximum Entropy Accuracy: " + str(MaxEntAccuracy))
 SVCaccuracy = classify.accuracy(SVCclassifier, test_set)
 print("SVC Accuracy: "+str(SVCaccuracy))
-
-#print (classifier.show_most_informative_features(10))
 
 #---------------------------------------------------------------------#
 
@@ -246,62 +215,3 @@ def classify_for_custom_input(custom_tweet: str):
 
     result_SVC = SVCclassifier.classify(custom_tweet_set)
     print("Result from SVC Classifier: ", result_SVC)
-
-
-# custom_tweet = "I hated the film. It was a disaster. Poor direction, bad acting."
-# custom_tweet_set = bag_of_words(custom_tweet)
-
-# result_Naive_Bayes = NBclassifier.classify(custom_tweet_set)
-# print ( result_Naive_Bayes) # Output: neg
-# Negative tweet correctly classified as negative
-
-# probability result for Naive Bayes
-# prob_result = NBclassifier.prob_classify(custom_tweet_set)
-# print (prob_result) # Output: <ProbDist with 2 samples>
-# print (prob_result.max()) # Output: neg
-# print (prob_result.prob("neg")) # Output: 0.941844352481
-# print (prob_result.prob("pos")) # Output: 0.0581556475194
-
-# result_MaxEnt = MaxEntClassifier.classify(custom_tweet_set)
-# print()
-
-# custom_tweet = "It was a wonderful and amazing movie. I loved it. Best direction, good acting."
-# custom_tweet_set = bag_of_words(custom_tweet)
-
-# print (classifier.classify(custom_tweet_set)) # Output: pos
-# Positive tweet correctly classified as positive
-
-# probability result
-# prob_result = NBclassifier.prob_classify(custom_tweet_set)
-# print (prob_result) # Output: <ProbDist with 2 samples>
-# print (prob_result.max()) # Output: pos
-# print (prob_result.prob("neg")) # Output: 0.00131055449755
-# print (prob_result.prob("pos")) # Output: 0.998689445502
-
-#---------------------------------------------------------------------#
-# from collections import defaultdict
-
-# actual_set = defaultdict(set)
-# predicted_set = defaultdict(set)
-
-# actual_set_cm = []
-# predicted_set_cm = []
-
-# for index, (feature, actual_label) in enumerate(test_set):
-#     actual_set[actual_label].add(index)
-#     actual_set_cm.append(actual_label)
-
-#     predicted_label = NBclassifier.classify(feature)
-
-#     predicted_set[predicted_label].add(index)
-#     predicted_set_cm.append(predicted_label)
-
-# from nltk.metrics import precision, recall, f_measure, ConfusionMatrix
-
-# print ('pos precision:', precision(actual_set['pos'], predicted_set['pos'])) # Output: pos precision: 0.762896825397
-# print ('pos recall:', recall(actual_set['pos'], predicted_set['pos'])) # Output: pos recall: 0.769
-# print ('pos F-measure:', f_measure(actual_set['pos'], predicted_set['pos'])) # Output: pos F-measure: 0.76593625498
-
-# print ('neg precision:', precision(actual_set['neg'], predicted_set['neg'])) # Output: neg precision: 0.767137096774
-# print ('neg recall:', recall(actual_set['neg'], predicted_set['neg'])) # Output: neg recall: 0.761
-# print ('neg F-measure:', f_measure(actual_set['neg'], predicted_set['neg'])) # Output: neg F-measure: 0.7640562249
